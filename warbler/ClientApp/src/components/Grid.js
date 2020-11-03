0e13bea109714f24b1621ef64c7ce01cae92c54e
sali6798 <shaidee@alingcastre.net>
2020-11-03 15:28:11 -0800

    created Grid component, display x on clicked square

---
+++ warbler/ClientApp/src/components/Grid.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Row from "./Row";
+
+export default function Grid() {
+    return (
+        <div style={{width: "70%", margin: "0 auto", padding: "20px 0"}}>
+            <Row />
+            <Row />
+            <Row />
+        </div>
+    );
+}
