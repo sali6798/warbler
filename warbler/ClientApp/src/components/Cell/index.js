665e56a91fa895985c72bfbe8401f06412c6aef1
sali6798 <shaidee@alingcastre.net>
2020-11-03 23:03:54 -0800

    display x or o based on playerId

--- warbler/ClientApp/src/components/Cell/index.js
+++ warbler/ClientApp/src/components/Cell/index.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+
 
 import "./style.css";
 
 export default function Cell(props) {
+    const [icon, setIcon] = useState();
+    const playerId = useSelector(state => state.playerId);
     const cross = (
         <>
             <line x1="10%" y1="10%" x2="90%" y2="90%"></line>
@@ -13,14 +17,17 @@ export default function Cell(props) {
     const circle = <circle cx="50%" cy="50%" r="40%"></circle>
 
     const handleClick = event => {
-        event.target.textContent = "x";
+        event.target.style.cursor = "default";
+        setIcon(playerId === 0 ? cross : circle)
     }
 
     return (
-        <div className="Cell">
-            <svg width="100%" height="100%">
-                {cross}
-            </svg>
+        <div className="Cell col-4">
+            <div className="svg-container">
+                <svg width="100%" height="100%" onClick={handleClick}>
+                    {icon}
+                </svg>
+            </div>
         </div>
     );
 }
