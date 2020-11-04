dac8b33e7f998b125991927e75b6c2ddbe70273d
sali6798 <shaidee@alingcastre.net>
2020-11-03 23:08:31 -0800

    setup for redux and redux chrome tool

--- warbler/ClientApp/src/index.js
+++ warbler/ClientApp/src/index.js
@@ -1,16 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import thunk from "redux-thunk";
+import { BrowserRouter } from "react-router-dom";
+import { applyMiddleware, compose, createStore } from "redux";
+import { Provider } from "react-redux";
+
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from "./store/reducers/reducer";
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   rootElement);
 
