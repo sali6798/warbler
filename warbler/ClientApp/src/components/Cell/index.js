29a4c23db6eb097c4e9ba477d70e8f271d75a6b5
sali6798 <shaidee@alingcastre.net>
2020-11-04 17:35:50 -0800

    display icon based on array value, change cursor style if clicked

--- warbler/ClientApp/src/components/Cell/index.js
+++ warbler/ClientApp/src/components/Cell/index.js
@@ -1,31 +1,27 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-
+import React, { useEffect } from "react";
 
 import "./style.css";
 
 export default function Cell(props) {
-    const [icon, setIcon] = useState();
-    const playerId = useSelector(state => state.playerId);
     const cross = (
         <>
             <line x1="10%" y1="10%" x2="90%" y2="90%"></line>
             <line x1="90%" y1="10%" x2="10%" y2="90%"></line>
         </>
     );
-
     const circle = <circle cx="50%" cy="50%" r="40%"></circle>
 
-    const handleClick = event => {
-        event.target.style.cursor = "default";
-        setIcon(playerId === 0 ? cross : circle)
-    }
+    useEffect(() => {
+        if (props.value !== -1) {
+            document.querySelector("#cell-" + props.id).style.cursor = "default";
+        }
+    }, [props.value])
 
     return (
         <div className="Cell col-4">
             <div className="svg-container">
-                <svg width="100%" height="100%" onClick={handleClick}>
-                    {icon}
+                <svg id={"cell-" + props.id} width="100%" height="100%" onClick={() => props.handleClick(props.value, props.id)}>
+                    {props.value === 0 ? circle : (props.value === 1 ? cross : null)}
                 </svg>
             </div>
         </div>
